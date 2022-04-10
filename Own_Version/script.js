1e69fd5e1046a6eaaeff4f44f813292aa95e9b8b
Juan Carlos Francisco Mesa <jcfm73@gmail.com>
2022-04-10 23:13:24 +0100

    formatted files

--- Own_Version/script.js
+++ Own_Version/script.js
@@ -1,100 +1,100 @@
 // jshint esversion:6
 
-let countyUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+let countyUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
-let educationUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
-
+let educationUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 
 let countyData;
 let educationData;
 
-let canvas = d3.select('#canvas');
+let canvas = d3.select("#canvas");
 
-let tooltip = d3.select('#tooltip');
+let tooltip = d3.select("#tooltip");
 
 let drawMap = () => {
+  canvas
+    .selectAll("path")
+    .data(countyData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
+    .attr("fill", (countyDataItem) => {
+      let id = countyDataItem.id;
 
-    canvas.selectAll('path')
-            .data(countyData)
-            .enter()
-            .append('path')
-            .attr('d',d3.geoPath())
-            .attr('class', 'county')
-            .attr('fill', (countyDataItem) => {
+      let county = educationData.find((item) => {
+        return item.fips === id;
+      });
 
-                let id = countyDataItem.id;
+      let percentage = county.bachelorsOrHigher;
+      if (percentage <= 15) {
+        return "tomato";
+      } else if (percentage <= 30) {
+        return "orange";
+      } else if (percentage <= 45) {
+        return "yellow";
+      } else if (percentage <= 60) {
+        return "limegreen";
+      } else {
+        return "blue";
+      }
+    })
+    .attr("data-fips", (countyDataItem) => {
+      return countyDataItem.id;
+    })
+    .attr("data-education", (countyDataItem) => {
+      let id = countyDataItem.id;
 
-                let county = educationData.find((item) => {
-                    return item.fips === id;
-                });
+      let county = educationData.find((item) => {
+        return item.fips === id;
+      });
 
-                let percentage = county.bachelorsOrHigher;
-                if (percentage <= 15) {
-                    return 'tomato';
-                } else if (percentage <= 30) {
-                    return 'orange';
-                }else if (percentage <= 45) {
-                    return 'yellow';
-                }else if (percentage <= 60) {
-                    return 'limegreen';
-                }else {
-                    return 'blue';
-                }
+      let percentage = county.bachelorsOrHigher;
+      return percentage;
+    })
+    .on("mouseover", (event, countyDataItem) => {
+      tooltip.transition().style("visibility", "visible");
 
-            })
-            .attr('data-fips', (countyDataItem) => {
-                return countyDataItem.id;
-            })
-            .attr('data-education', (countyDataItem) => {
+      let id = countyDataItem.id;
 
-                let id = countyDataItem.id;
+      let county = educationData.find((item) => {
+        return item.fips === id;
+      });
 
-                let county = educationData.find((item) => {
-                    return item.fips === id;
-                });
-
-                let percentage = county.bachelorsOrHigher;
-                return percentage;
-            })
-            .on('mouseover', (event, countyDataItem) => {
-                tooltip.transition()
-                    .style('visibility','visible');
-
-                let id = countyDataItem.id;
-
-                let county = educationData.find((item) => {
-                    return item.fips === id;
-                });
-
-                // console.log(event);
-
-                tooltip.text(county.fips + ' - ' + county.area_name + ', ' + county.state + ' : ' + county.bachelorsOrHigher + '%')
-                .attr('data-education', county.bachelorsOrHigher);
-            })
-            .on('mouseout', (countyDataItem) => {
-                tooltip.transition()
-                    .style('visibility','hidden');
-            })
-            ;
-
+      tooltip
+        .text(
+          county.fips +
+            " - " +
+            county.area_name +
+            ", " +
+            county.state +
+            " : " +
+            county.bachelorsOrHigher +
+            "%"
+        )
+        .attr("data-education", county.bachelorsOrHigher);
+    })
+    .on("mouseout", (countyDataItem) => {
+      tooltip.transition().style("visibility", "hidden");
+    });
 };
 
-d3.json(countyUrl)
-    .then((data, error) => {
-        if (error) {
-            console.log(error);
-        }else{
-            countyData = topojson.feature(data, data.objects.counties).features;
+d3.json(countyUrl).then((data, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    countyData = topojson.feature(data, data.objects.counties).features;
 
-            d3.json(educationUrl)
-                .then((data, error) => {
-                    if (error) {
-                        console.log(error);
-                    }else{
-                        educationData = data;
+    d3.json(educationUrl).then((data, error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        educationData = data;
 
-                        drawMap();
-                    }
-                });
-        }
+        drawMap();
+      }
     });
+  }
+});
