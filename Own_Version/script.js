79995b1b0ee1ce1e76afc05bb2ea398f999dd668
Juan Carlos Francisco Mesa <jcfm73@gmail.com>
2022-04-12 14:14:39 +0100

    Comments added.  All tests Ok

--- Own_Version/script.js
+++ Own_Version/script.js
@@ -1,5 +1,20 @@
 // jshint esversion:6
 
+/**
+ *  Coded by: Juan Carlos Francisco Mesa.
+ * 
+ * 
+ * I followed the video at: https://www.youtube.com/watch?v=ha1toFtBfF8 and made some changes in order to personalize the result.
+ * 
+ *  You can see the code at: 
+ * 
+ * https://codepen.io/jcfr4n/full/YzYLEoZ or 
+ * https://github.com/jcfr4n/corophlet_map_freecodecamp
+ * 
+ */
+
+// Init vars.
+
 let countyUrl =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
@@ -9,7 +24,11 @@ let educationUrl =
 let countyData;
 let educationData;
 
+// Define canvas element
+
 let canvas = d3.select("#canvas");
+
+// Define tooltip element
 
 let tooltip = d3
   .select("#main")
@@ -17,12 +36,15 @@ let tooltip = d3
   .attr("id", "tooltip")
   .style("opacity", 0);
 
+// Define the function drawMap that is called once the data was downloaded from theri respectives pages
+
 let drawMap = () => {
   canvas
     .selectAll("path")
     .data(countyData)
     .enter()
     .append("path")
+    // This is a function from topojson library, which create the points that define each county
     .attr("d", d3.geoPath())
     .attr("class", "county")
     .attr("fill", (countyDataItem) => {
@@ -58,6 +80,7 @@ let drawMap = () => {
       let percentage = county.bachelorsOrHigher;
       return percentage;
     })
+    // Define the behavoir when the mouse is over each county
     .on("mouseover", (event, countyDataItem) => {
       tooltip.transition().duration(500).style("opacity", 0.9);
 
@@ -66,7 +89,7 @@ let drawMap = () => {
       let county = educationData.find((item) => {
         return item.fips === id;
       });
-
+// This makes visible the tooltips
       tooltip
         .html(
           county.fips +
@@ -82,11 +105,12 @@ let drawMap = () => {
         .style("left", event.pageX + 30 + "px")
         .style("top", event.pageY - 30 + "px");
     })
+    // Define the behavoir when the mouse is out of each county
     .on("mouseout", (countyDataItem) => {
       tooltip.transition().style("opacity", 0);
     });
 };
-
+// this init the request of the data and evently call the drawMap function
 d3.json(countyUrl).then((data, error) => {
   if (error) {
     console.log(error);
